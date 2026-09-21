a25a3cb7c698d39a980ab3a93ca342467b83231e
Priyansh <electroniorje@gmail.com>
2026-09-21 10:17:06 +0530

    feat: Rebase codebase

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,8 @@
-import express from "express";
-import cors from "cors";
+import "dotenv/config";
+import app from "./app.js";
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.json({
-        message: "Velllm server is running"
-    });
-});
-
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
